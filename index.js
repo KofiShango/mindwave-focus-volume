72a10f208a2807227ad17f41261653814e124e3b
Kofi Shango <kofishango@gmail.com>
2021-03-13 23:43:44 -0500

    add comments

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const { audio } = require('system-control');
 const brain = createClient({ enableRawOutput: false });
 const debounce = require('debounce');
 
+// focus mode is on by default
 let focus = true;
 let blink = false;
+// Volume high or low
+let high = false;
+
 if(process.argv.length > 2){
-    console.log(process.argv[2] === '-blink')
-    blink = process.argv[2] === '-blink'
+    blink = process.argv[2] === '-blink';
 }
+
+/**
+ * Debounce toggle by 5 secs
+ * to keep from spamming volume change
+ */
 const toggle = debounce(()=>{
     if(high){
         audio.volume(99)
@@ -18,10 +26,11 @@ const toggle = debounce(()=>{
     high = !high
 }, 5000, true);
 
-brain.connect();
+// default volume to 50%
 audio.volume(50)
-let high = false
+brain.connect();
 
+// Focus logic
 if(focus){
     brain.on('data', data=>{
         const attention = data && data.eSense && data.eSense.attention 
@@ -31,13 +40,12 @@ if(focus){
     })
 }
 
+// Blink logic
 if(blink){
     brain.on('blink_data', data=>{
-        console.log(data)
         const blinkStrength = data && data.blinkStrength
         if(blinkStrength > 65){
             toggle(attention)
         }
     })
 }
-
